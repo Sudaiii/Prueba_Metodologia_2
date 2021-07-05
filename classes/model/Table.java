7cb8de223c67fb270b97699de95b05bda4175229
Elías Ocque <elias.ocque2@gmail.com>
2021-07-05 12:17:47 -0400

    Creacion inicial del tablero

--- classes/model/Table.java
+++ classes/model/Table.java
@@ -1,5 +1,22 @@
 package classes.model;
 
-public class Table{
+import java.util.Random;
 
+public class Table{
+    int[][] grid;
+
+    public Table(int size){
+        grid = new int[size][size];
+    }
+
+    private void fillCovid(){
+        Random rng = new Random();
+        for(int[] columna: grid){
+            for(int casilla: columna){
+                if(rng.nextInt(4) <= 1){
+                    casilla = 9;
+                }
+            }
+        }
+    }
 }
