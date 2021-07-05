d154c84cdf40b6853cd8b89f0889e92b3c7cd287
Nacho <84586500+LuisMedinaBravo@users.noreply.github.com>
2021-07-05 12:41:42 -0400

    Add files via upload
    Interfaz

---
+++ classes/Main.java
@@ -0,0 +1,14 @@
+
+package BuscaMina;
+
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String args[]){
+        
+        Menu ventana = new Menu();
+        ventana.setVisible(true);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+    }
+}
