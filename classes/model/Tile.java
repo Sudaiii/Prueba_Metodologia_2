f02f632cecbfd00bb15a07054efee3d73a9bedc8
Elías Ocque <elias.ocque2@gmail.com>
2021-07-05 12:42:49 -0400

    Implementado Table y Tile

---
+++ classes/model/Tile.java
@@ -0,0 +1,31 @@
+package classes.model;
+
+public class Tile {
+    protected boolean state;
+    protected int value;
+
+    public Tile() {
+        state = false;
+        value = 0;
+    }
+    
+    public boolean isRevealed() {
+        return state;
+    }
+
+    public int getAdyacentCovid() {
+        return value;
+    }
+
+    public void reveal() {
+        state = true;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void sumOne(){
+        value++;
+    }
+}
